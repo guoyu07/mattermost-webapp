aee6d5ad9b9e9691710e2de146661c4ecff2b908
Harrison Healey <harrisonmhealey@gmail.com>
2018-01-22 10:30:27 -0500

    ICU-613 Switched all post components to use PostTime

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -1263,13 +1263,6 @@ export function windowHeight() {
     return $(window).height();
 }
 
-export function getWindowDimensions() {
-    return {
-        width: window.innerWidth,
-        height: window.innerHeight
-    };
-}
-
 export function getChannelTerm(channelType) {
     let channelTerm = 'Channel';
     if (channelType === Constants.PRIVATE_CHANNEL) {
